92b168c68b4f6e1374b518d5bfc88053ad270dc3
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2021-12-11 14:55:40 +0300

    add Javasctript file

---
+++ script.js
@@ -0,0 +1,7 @@
+let day = document.getElementById("Day")
+let month = document.getElementById("Month")
+let year = document.getElementById("Year")
+let gender = document.getElementById("Gender")
+let submit = document.getElementById("Submit")
+
+
