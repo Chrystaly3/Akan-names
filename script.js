06002cd642cd4c4cafe2ae11834885ec340428a1
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2021-12-11 15:42:41 +0300

    validated input

--- script.js
+++ script.js
@@ -4,4 +4,25 @@ let year = document.getElementById("Year")
 let gender = document.getElementById("Gender")
 let submit = document.getElementById("Submit")
 
+ function AkanName(){
+   if(day<=0 || day>31){
+     alert("Josephine says invalid!")
+    }
+    else{
+      if(month<=0 || month>12){
+        alert("Josephine says invalid!")
+      }
+      else{
+        if(year<=0 || year>2021){
+          alert("Josephine says invalid")
+        }
+        else{
+          
+          var AkanMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+          var AkanFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
+        }
+      }
+    }
+  }
+       
