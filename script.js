10f943c82022583266c8e6a9babcff01a1fb5e7e
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2021-12-12 21:25:47 +0300

    modified

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let day = document.getElementById("Day")
 let month = document.getElementById("Month")
 let year = document.getElementById("Year")
-let gender = document.getElementById("Gender")
+let Male = document.getElementById("Male")
+let Female = document.getElementById("Female")
 let submit = document.getElementById("Submit")
 
  function AkanName(){
-   alert(day.value)
+   
   if(day.value<=0 || day.value>31){
      alert("Josephine says invalid!")
     }
@@ -18,17 +19,24 @@ let submit = document.getElementById("Submit")
           alert("Josephine says invalid")
         }
         else{
-          
+          let gender =""
+            if(Male.checked){
+                gender = "Male"
+            }
+            else if(Female.checked){
+                gender = "Female"
+            }
+                    
           var AkanMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
           var AkanFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
           var date = new Date(year.value+"/"+month.value+"/"+day.value)
           var display=document.getElementById("display")
           var Akan= date.getDay()
-          if(gender.value=="Male"){
-           alert("Josephine says your Akan name is"+ AkanMale[Akan])
+          if(gender=="Male"){
+           alert("Josephine says your Akan name is "+ AkanMale[Akan])
           }
-            else  if(gender.value=="Female"){
-              alert("Josephine says your Akan name is"+ AkanFemale[Akan])
+            else  if(gender=="Female"){
+              alert("Josephine says your Akan name is "+ AkanFemale[Akan])
             }else{
               alert("Josephine says invalid")
             }
